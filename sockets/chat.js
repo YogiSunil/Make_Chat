d5c36831c8047c2c947c629209c23b3b79d35551
YogiSunil <nathsunil2021@outlook.com>
2025-09-30 21:33:39 -0500

    Users can log in

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,55 +1,11 @@
-// Chat socket handlers
-const chatHandlers = (io) => {
-    io.on('connection', (socket) => {
-        console.log('A user connected:', socket.id);
+// chat.js
+module.exports = (io, socket) => {
 
-        // Handle chat messages
-        socket.on('chat message', (msg) => {
-            console.log('Message received:', msg);
-            // Broadcast message to all connected clients
-            io.emit('chat message', {
-                id: socket.id,
-                message: msg,
-                timestamp: new Date().toLocaleTimeString()
-            });
-        });
+  // Listen for "new user" socket emits
+  socket.on('new user', (username) => {
+    console.log(`✋ ${username} has joined the chat! ✋`);
+    //Send the username to all clients currently connected
+    io.emit("new user", username);
+  })
 
-        // Handle user joining
-        socket.on('user joined', (username) => {
-            socket.username = username;
-            socket.broadcast.emit('user joined', {
-                username: username,
-                message: `${username} joined the chat`
-            });
-        });
-
-        // Handle user leaving
-        socket.on('user left', (username) => {
-            socket.broadcast.emit('user left', {
-                username: username,
-                message: `${username} left the chat`
-            });
-        });
-
-        // Handle typing indicators
-        socket.on('typing', (data) => {
-            socket.broadcast.emit('typing', {
-                username: data.username,
-                isTyping: data.isTyping
-            });
-        });
-
-        // Handle disconnect
-        socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-            if (socket.username) {
-                socket.broadcast.emit('user left', {
-                    username: socket.username,
-                    message: `${socket.username} left the chat`
-                });
-            }
-        });
-    });
-};
-
-module.exports = chatHandlers;
+}
